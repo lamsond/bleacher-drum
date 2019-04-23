f736bc2198322aa124d9d87505864c24fd7dfe69
conrad <lamsond@wctech.org>
2019-04-22 21:34:34 -0400

    updated event listeners for new_game real time updates

--- lineup_site/lineup_app/static/lineup_app/realtime-scoreboard.js
+++ lineup_site/lineup_app/static/lineup_app/realtime-scoreboard.js
@@ -13,11 +13,28 @@ const away_team_sb = document.querySelector('#away-team-sb');
 const home_team_sb = document.querySelector('#home-team-sb');
  
 // event handlers
-function updateHomeTeam(e){
+function update_home_team(e){
 	let team = home_team.options[this.selectedIndex].text;
 	home_team_sb.innerHTML = team;
 }
- 
-// set up event listeners
-home_team.addEventListener('change', updateHomeTeam);
 
+function update_away_team(e){
+	let team = away_team.options[this.selectedIndex].text;
+	away_team_sb.innerHTML = team;
+}
+
+function update_date(e){
+	const date_str = `${month.value}/${day.value}/${year.value}`;
+	gameday.innerHTML = date_str;
+}
+
+// set up event listeners
+home_team.addEventListener('change', update_home_team);
+away_team.addEventListener('change', update_away_team);
+month.addEventListener('change', update_date);
+//month.addEventListener('load', update_date);
+day.addEventListener('change', update_date);
+//day.addEventListener('load', update_date);
+year.addEventListener('change', update_date);
+//year.addEventListener('load', update_date);
+window.addEventListener('load', update_date);
