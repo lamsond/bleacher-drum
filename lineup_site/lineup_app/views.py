4c6510fbfec36b837524f0925634efdb08163178
conrad <lamsond@wctech.org>
2019-06-15 17:20:03 -0400

    stats v

--- lineup_site/lineup_app/views.py
+++ lineup_site/lineup_app/views.py
@@ -103,16 +103,3 @@ def view_players(request):
     context = {'players': players}
     return render(request, 'lineup_app/view_players.html', context)
 
-def noodle(request, game_id):
-    game = get_object_or_404(Game, pk=game_id)
-    nyy = Team.objects.get(abbr='NYY')
-    players = Player.objects.filter(team=nyy)
-    positions = ('P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'DH', 'PH')
-    slot_indices = range(1, 10)
-    context = {'game': game,
-            'players': players,
-            'positions': positions,
-            'slots': slot_indices,
-            }
-
-    return render(request, 'lineup_app/set_lineup_style.html', context)
